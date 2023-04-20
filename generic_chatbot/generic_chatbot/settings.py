8d7d566cd64d52348493849168a6bb9e1852397d
Shivin Chengappa <chengshiv@gmail.com>
2023-04-20 12:48:26 +0530

    Update settings.py

--- generic_chatbot/generic_chatbot/settings.py
+++ generic_chatbot/generic_chatbot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ofhhuiij@+l7du731ip0qtg0v^hd*0qawsl2-dhjb2=1r^^ea^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
